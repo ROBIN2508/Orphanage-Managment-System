b3293c2dc1706c04b31f4862a2fcecbcc71bd406
ROBIN2508 <65755953+ROBIN2508@users.noreply.github.com>
2021-05-21 23:58:58 +0800

    Update add_orphanage.php

--- account/add_orphanage.php
+++ account/add_orphanage.php
@@ -1,1 +1,398 @@
+<?php
+    // 1 hours in seconds
+    $inactive = 60*60; 
+    ini_set('session.gc_maxlifetime', $inactive); // set the session max lifetime to 1 hours
+        
+    session_start();
+    if (isset($_SESSION['admin_id'])){
+        require_once "../db_connection.php";
 
+        $admin_id = $_SESSION['admin_id'];
+
+        if (isset($_SESSION['testing']) && (time() - $_SESSION['testing'] > $inactive)) {
+            // last request was more than 1 hours ago
+            session_unset();     // unset $_SESSION variable for this page
+            session_destroy();   // destroy session data
+        }
+
+        $_SESSION['admin_id'] = $admin_id;
+        $_SESSION['testing'] = time(); // Update session
+
+        $sql = "SELECT admin_id, name, father_name, address, mobile_no, email, password,acc_type FROM orp_admin WHERE admin_id = '$admin_id'";
+        $result = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_assoc($result)){
+            $user_id = $row["admin_id"];
+            $name = $row["name"];
+            $father_name = $row["father_name"];
+            $email = $row["email"];
+            $mobile_no = $row["mobile_no"];
+            $address = $row["address"];
+            $password = $row["password"];
+            $acc_type = $row["acc_type"];
+
+            if ($acc_type==1){
+                header("Location: ./index_user.php");
+            }
+        }
+    }
+    
+    else{
+        header("Location: ../signin");
+    }
+
+    if (isset($_POST["logout"])){
+        session_unset();     // unset $_SESSION variable for this page
+        session_destroy();   // destroy session data
+        header("Location: ../signin");
+    }
+
+
+    if (isset($_POST["add_orp_data"])){
+        $sql="SELECT MAX(Cast(right(orp_id,3) as int)) FROM orp_details";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_row($result);
+        $highest_id = $row[0];
+        if($highest_id==null){
+            $highest_id=date('y')."000";
+        }
+        if($highest_id>0 && $highest_id<10){
+            $highest_id=date('y')."00".$highest_id;
+        }
+        if($highest_id>9 && $highest_id<100){
+            $highest_id=date('y')."0".$highest_id;
+        }
+
+        $orp_id=($highest_id+1);
+
+        $orp_name=$_POST["inputname"];
+        $orp_dob=$_POST["inputdob"];
+        $orp_gender=$_POST["inputgender"];
+        $orp_status=0;
+        
+        // echo $orp_id.' '.$orp_name.' '. $orp_dob.' ' . $orp_gender.' '.$orp_status;
+        $sql="INSERT INTO orp_details (orp_id, name, dob, gender, status) VALUES ('$orp_id','$orp_name','$orp_dob','$orp_gender','$orp_status')";
+        mysqli_query($conn,$sql);
+        header("Location: ./");
+
+
+    }
+
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>View All Orphanage || Orphanage management system </title>
+    <!-- HTML5 Shim and Respond.js IE11 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 11]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- Favicon icon -->
+    <!-- <link rel="icon" href="assets/images/favicon.svg" type="image/x-icon"> -->
+
+    <!-- font css -->
+    <link rel="stylesheet" href="assets/fonts/feather.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome.css">
+    <link rel="stylesheet" href="assets/fonts/material.css">
+
+    <!-- vendor css -->
+    <link rel="stylesheet" href="assets/css/style.css" id="main-style-link">
+
+</head>
+
+<body class="">
+    <!-- [ Pre-loader ] start -->
+    <div class="loader-bg">
+        <div class="loader-track">
+            <div class="loader-fill"></div>
+        </div>
+    </div>
+    <!-- [ Pre-loader ] End -->
+    <!-- [ Mobile header ] start -->
+    <div class="pc-mob-header pc-header d-sm-flex d-lg-none">
+        <div class="pcm-toolbar  d-md-flex flex-sm-row">
+            <a class="pc-head-link" id="mobile-collapse">
+                <div class="hamburger hamburger--arrowturn">
+                    <div class="hamburger-box">
+                        <div class="hamburger-inner"></div>
+                    </div>
+                </div>
+            </a>
+            <!-- <a href="#!" class="pc-head-link" id="headerdrp-collapse">
+                <i data-feather="align-right"></i>
+            </a> -->
+        </div>
+        <div class="pcm-toolbar d-sm-flex justify-content-sm-end">
+            <a class="pc-head-link " id="header-collapse">
+                <i data-feather="more-vertical"></i>
+            </a>
+        </div>
+    </div>
+    <!-- [ Mobile header ] End -->
+
+    <!-- [ navigation menu ] start -->
+    <nav class="pc-sidebar ">
+        <div class="navbar-wrapper">
+            <div class="m-header h6 text-center">
+                <a href="index.html" class="b-brand">
+                    <!-- ========   change your logo hear   ============ -->
+                    <img src="" alt="Orphanage management system" class="logo logo-lg">
+                    <img src="" alt="Orphanage management system" class="logo logo-sm">
+                </a>
+            </div>
+            <div class="navbar-content">
+                <ul class="pc-navbar mt-2">
+                    <!-- <li class="pc-item pc-caption">
+                                        <label>Admin</label>
+                                    </li> -->
+                    <li class="pc-item">
+                        <a href="./" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">home</i>
+                            </span>
+                            <span class="pc-mtext">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="pc-item pc-caption">
+                        <label>Orphanage</label>
+                        <!-- <span>Tones of readymade charts</span> -->
+                    </li>
+
+                    <!-- Add new Orphanage -->
+                    <li class="pc-item active">
+                        <a class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Add Orphanage</span>
+                        </a>
+                    </li>
+
+                    <!-- All Orphanage -->
+                    <li class="pc-item">
+                        <a href="./all_orphanage.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">All Orphanage</span>
+                        </a>
+                    </li>
+                    <!-- Only Available Orphanage -->
+                    <li class="pc-item">
+                        <a href="./avail_orphanage.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Available Orphanage</span>
+                        </a>
+                    </li>
+
+                    <!-- Only Windrow Orphanage -->
+                    <!-- <li class="pc-item">
+                        <a href="./widrow_orphanage.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Widrowed Orphanage</span>
+                        </a>
+                    </li> -->
+                    <!-- Admin & User -->
+                    <li class="pc-item pc-caption">
+                        <label>User &amp; Admin</label>
+                        <!-- <span>Tones of readymade charts</span> -->
+                    </li>
+                    <!-- All User -->
+                    <li class="pc-item">
+                        <a href="./users.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Users</span>
+                        </a>
+                    </li>
+                    <!-- All Admin -->
+                    <li class="pc-item">
+                        <a href="./admin.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Admins</span>
+                        </a>
+                    </li>
+
+                    <!-- Donation & Windrow -->
+                    <li class="pc-item pc-caption">
+                        <label>Donation &AMP; Widrow</label>
+                    </li>
+
+                    <!-- Donation -->
+                    <li class="pc-item">
+                        <a href="./donation.php" class="pc-link ">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Donation</span>
+                        </a>
+                    </li>
+                    <!-- Windrow -->
+                    <li class="pc-item">
+                        <a href="./widrow_request.php" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="material-icons-two-tone">table_rows</i>
+                            </span>
+                            <span class="pc-mtext">Widrow Requests</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- [ navigation menu ] end -->
+    <!-- [ Header ] start -->
+    <header class="pc-header ">
+        <div class="header-wrapper">
+            <div class="ml-auto">
+                <ul class="list-unstyled">
+                    <li class="dropdown pc-h-item">
+                        <a class="pc-head-link dropdown-toggle arrow-none mr-0" data-toggle="dropdown" role="button"
+                            aria-haspopup="false" aria-expanded="false">
+                            <img src="../source/img/profile.png" alt="user-image" class="user-avtar">
+                            <span>
+                                <span class="user-name"><?php echo $name; ?></span>
+                                <span class="user-desc">
+                                    <?php 
+                                        if ($acc_type == 1){
+                                            echo "User";
+                                        }
+                                        if ($acc_type == 0){
+                                            echo "Administrator";
+                                        }
+                                    ?>    
+                                </span>
+                            </span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right pc-h-dropdown">
+                            <form action="" method="POST">
+                                <button class="dropdown-item" type="submit" name="logout">
+                                    <i class="material-icons-two-tone">chrome_reader_mode</i>
+                                    <span>Logout</span>
+                                </button>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </header>
+    <!-- [ Header ] end -->
+
+    <!-- [ Main Content ] start -->
+    <div class="pc-container">
+        <div class="pcoded-content">
+            <!-- [ breadcrumb ] start -->
+            <div class="page-header">
+                <div class="page-block">
+                    <div class="row align-items-center">
+                        <div class="col-md-6">
+                            <div class="page-header-title">
+                                <h5 class="m-b-10">Add Orphanage</h5>
+                            </div>
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a href="./">Home</a>
+                                </li>
+                                <li class="breadcrumb-item">Dashboard</li>
+                                <li class="breadcrumb-item">Add Orphanage</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- [ breadcrumb ] end -->
+            <!-- [ Main Content ] start -->
+            <div class="row">
+                <!-- support-section start -->
+                <div class="d-flex justify-content-center">
+                    <form action="" method="POST">
+                        <div class="row mb-3">
+                            <label for="inputname" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputname" name="inputname" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputdob" class="col-sm-2 col-form-label">DoB</label>
+                            <div class="col-sm-10">
+                                <input type="date" class="form-control" id="inputdob" name="inputdob" required>
+                            </div>
+                        </div>
+                        <fieldset class="row mb-3">
+                            <legend class="col-sm-2 col-form-label">Gender</legend>
+                            <div class="col-sm-10">
+                                <input class="form-control" list="gender_detail" name="inputgender" required>
+			                    <datalist id="gender_detail"><option value="Male"><option value="Female"><option value="Other"></datalist>
+                            </div>
+                        </fieldset>
+                        <button type="submit" class="btn btn-primary" name="add_orp_data">Add</button>
+                    </form>
+                </div>
+                <!-- support-section end -->
+            </div>
+            <!-- [ Main Content ] end -->
+        </div>
+    </div>
+    <!-- [ Main Content ] end -->
+    <!-- Warning Section start -->
+    <!-- Older IE warning message -->
+    <!--[if lt IE 11]>
+        <div class="ie-warning">
+            <h1>Warning!!</h1>
+            <p>You are using an outdated version of Internet Explorer, please upgrade
+               <br/>to any of the following web browsers to access this website.
+            </p>
+            <div class="iew-container">
+                <ul class="iew-download">
+                    <li>
+                        <a href="http://www.google.com/chrome/">
+                            <img src="assets/images/browser/chrome.png" alt="Chrome">
+                            <div>Chrome</div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.mozilla.org/en-US/firefox/new/">
+                            <img src="assets/images/browser/firefox.png" alt="Firefox">
+                            <div>Firefox</div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <p>Sorry for the inconvenience!</p>
+        </div>
+    <![endif]-->
+    <!-- Warning Section Ends -->
+    <!-- Required Js -->
+    <script src="assets/js/vendor-all.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/feather.min.js"></script>
+    <script src="assets/js/pcoded.min.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script> -->
+    <!-- <script src="assets/js/plugins/clipboard.min.js"></script> -->
+    <!-- <script src="assets/js/uikit.min.js"></script> -->
+
+    <!-- Apex Chart -->
+    <script src="assets/js/plugins/apexcharts.min.js"></script>
+
+    <!-- custom-chart js -->
+    <script src="assets/js/pages/dashboard-sale.js"></script>
+</body>
+
+</html>
